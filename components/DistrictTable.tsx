3002374c088b775ac8632a8d808b1c35eda3c371
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-04-28 15:49:31 +0530

    Fixed bug with resize of the table as per width

--- components/DistrictTable.tsx
+++ components/DistrictTable.tsx
@@ -21,10 +21,79 @@ interface TableData {
 
 const DistrictTable = ({ data }: DistrictTableProps) => {
   const [tableData, setTabledata] = useState<TableData[]>([]);
-  const [width, setWidth] = useState(0);
+  const [columns, setColumns] = useState<ColumnsType<TableData>>([]);
 
   useEffect(() => {
-    if (window) setWidth(window.innerWidth);
+    if (window) {
+      const columns: ColumnsType<TableData> = [
+        {
+          title: "DISTRICT",
+          width: window.innerWidth > 500 ? 200 : 130,
+          dataIndex: "district",
+          key: "district",
+          fixed: "left",
+          sorter: (a, b) => a.district.localeCompare(b.district),
+          ellipsis: true,
+        },
+        {
+          title: "CONFIRMED",
+          width: 130,
+          dataIndex: "confirmed",
+          key: "confirmed",
+          sorter: (a, b) => a.confirmed - b.confirmed,
+        },
+        {
+          title: "ACTIVE",
+          dataIndex: "active",
+          key: "active",
+          width: 130,
+          sorter: (a, b) => a.active - b.active,
+        },
+        {
+          title: "RECOVERED",
+          dataIndex: "recovered",
+          key: "recovered",
+          width: 130,
+          sorter: (a, b) => a.recovered - b.recovered,
+        },
+        {
+          title: "DEATHS",
+          dataIndex: "deceased",
+          key: "deceased",
+          width: 130,
+          sorter: (a, b) => a.deceased - b.deceased,
+        },
+        {
+          title: "UNDER OBSERVATION",
+          dataIndex: "total_obs",
+          key: "total_obs",
+          width: window.innerWidth > 500 ? 150 : 130,
+          sorter: (a, b) => a.total_obs - b.total_obs,
+        },
+        {
+          title: "HOME ISOLATION",
+          dataIndex: "home_obs",
+          key: "home_obs",
+          width: window.innerWidth > 500 ? 150 : 130,
+          sorter: (a, b) => a.home_obs - b.home_obs,
+        },
+        {
+          title: "HOSPITAL ISOLATION",
+          dataIndex: "hospital_obs",
+          key: "hospital_obs",
+          width: window.innerWidth > 500 ? 150 : 130,
+          sorter: (a, b) => a.hospital_obs - b.hospital_obs,
+        },
+        {
+          title: "HOSPITALIZED TODAY",
+          dataIndex: "hospital_today",
+          key: "hospital_today",
+          width: window.innerWidth > 500 ? 150 : 140,
+          sorter: (a, b) => a.hospital_today - b.hospital_today,
+        },
+      ];
+      setColumns(columns);
+    }
     const tempTableData: TableData[] = [];
     Object.keys(data.summary).map((district, key) => {
       tempTableData.push({
@@ -44,74 +113,6 @@ const DistrictTable = ({ data }: DistrictTableProps) => {
     });
     setTabledata(tempTableData);
   }, []);
-
-  const columns: ColumnsType<TableData> = [
-    {
-      title: "DISTRICT",
-      width: width > 500 ? 200 : 130,
-      dataIndex: "district",
-      key: "district",
-      fixed: "left",
-      sorter: (a, b) => a.district.localeCompare(b.district),
-      ellipsis: true,
-    },
-    {
-      title: "CONFIRMED",
-      width: 130,
-      dataIndex: "confirmed",
-      key: "confirmed",
-      sorter: (a, b) => a.confirmed - b.confirmed,
-    },
-    {
-      title: "ACTIVE",
-      dataIndex: "active",
-      key: "active",
-      width: 130,
-      sorter: (a, b) => a.active - b.active,
-    },
-    {
-      title: "RECOVERED",
-      dataIndex: "recovered",
-      key: "recovered",
-      width: 130,
-      sorter: (a, b) => a.recovered - b.recovered,
-    },
-    {
-      title: "DEATHS",
-      dataIndex: "deceased",
-      key: "deceased",
-      width: 130,
-      sorter: (a, b) => a.deceased - b.deceased,
-    },
-    {
-      title: "UNDER OBSERVATION",
-      dataIndex: "total_obs",
-      key: "total_obs",
-      width: width > 500 ? 150 : 130,
-      sorter: (a, b) => a.total_obs - b.total_obs,
-    },
-    {
-      title: "HOME ISOLATION",
-      dataIndex: "home_obs",
-      key: "home_obs",
-      width: width > 500 ? 150 : 130,
-      sorter: (a, b) => a.home_obs - b.home_obs,
-    },
-    {
-      title: "HOSPITAL ISOLATION",
-      dataIndex: "hospital_obs",
-      key: "hospital_obs",
-      width: width > 500 ? 150 : 130,
-      sorter: (a, b) => a.hospital_obs - b.hospital_obs,
-    },
-    {
-      title: "HOSPITALIZED TODAY",
-      dataIndex: "hospital_today",
-      key: "hospital_today",
-      width: width > 500 ? 150 : 140,
-      sorter: (a, b) => a.hospital_today - b.hospital_today,
-    },
-  ];
 
   return (
     <>
